45b927a06da05c8a22918f12e4265a627709072d
ashcl <christopher.lee.ash@gmail.com>
2018-02-01 20:02:06 -0500

    Basic Swing Hello World window

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public Main(){
+        this.setTitle("Cryptonite");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel lblHello = new JLabel("Hello World! Let's get cracking!");
+        Container contentPane = this.getContentPane();
+        contentPane.add(lblHello);
+        this.setSize(800, 600);
+        this.setVisible(true);
+
+    }
+
 }
