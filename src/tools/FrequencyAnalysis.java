e92f521b60d9998024f6b2a770b6f1a9e0b3f1fe
ashcl <christopher.lee.ash@gmail.com>
2018-02-09 13:01:27 -0500

    Implemented monogram frequency analysis.

--- src/tools/FrequencyAnalysis.java
+++ src/tools/FrequencyAnalysis.java
@@ -1,23 +1,42 @@
 package tools;
 
-import java.util.Map;
+import java.util.HashMap;
 
 public class FrequencyAnalysis {
 
-    private Map<String, Integer> frequencyChart;
-    FrequencyAnalysis(){
-
+    public static void main(String[] args){
+        String cipher = "abcdefghijklzmnopqrstuvwxyz";
+        HashMap<String, Integer> monograms = findMonograms(cipher);
+        StringBuilder builder = new StringBuilder();
+        for (String key :
+                monograms.keySet()) {
+            builder.append(key + ": " + monograms.get(key) + "\n");
+        }
+        System.out.println(builder.toString());
     }
 
-    public static Map<String, Integer> findMonograms(String cipher){
+    public static HashMap<String, Integer> findMonograms(String cipher){
+        HashMap<String, Integer> frequencyChart = new HashMap<>();
+        cipher.replace(" ", "");
+        String lowerCipher = cipher.toLowerCase();
+        for (int i = 0; i < lowerCipher.length(); i++){
+            String character = Character.toString(lowerCipher.charAt(i));
+            if(frequencyChart.containsKey(character)){
+                int frequency = frequencyChart.get(character);
+                frequency++;
+                frequencyChart.put(character, frequency);
+            }else{
+                frequencyChart.put(character, 1);
+            }
+        }
+        return frequencyChart;
+    }
+
+    public static HashMap<String, Integer> findDigrams(String cipher){
         return null;
     }
 
-    public static Map<String, Integer> findDigrams(String cipher){
-        return null;
-    }
-
-    public static Map<String, Integer> findTrigrams(String cipher){
+    public static HashMap<String, Integer> findTrigrams(String cipher){
         return null;
     }
 
