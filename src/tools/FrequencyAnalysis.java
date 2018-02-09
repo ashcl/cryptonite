c296b3f3c25cde3d9ab37ef1aa4ca8f08fcc5f70
ashcl <christopher.lee.ash@gmail.com>
2018-02-09 11:37:11 -0500

    Basic Swing Hello World window

---
+++ src/tools/FrequencyAnalysis.java
@@ -0,0 +1,24 @@
+package tools;
+
+import java.util.Map;
+
+public class FrequencyAnalysis {
+
+    private Map<String, Integer> frequencyChart;
+    FrequencyAnalysis(){
+
+    }
+
+    public static Map<String, Integer> findMonograms(String cipher){
+        return null;
+    }
+
+    public static Map<String, Integer> findDigrams(String cipher){
+        return null;
+    }
+
+    public static Map<String, Integer> findTrigrams(String cipher){
+        return null;
+    }
+
+}
