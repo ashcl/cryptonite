1922f738932355ea9ab7df589dde476260843f1d
ashcl <christopher.lee.ash@gmail.com>
2018-02-23 12:43:09 -0500

    Added Kasiski Analysis file and Caesar Cipher window.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import visuals.CaesarCipherWindow;
 import visuals.FrequencyWindow;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main extends JFrame{
+public class Main extends JFrame implements ActionListener{
 
     //Child Windows
     FrequencyWindow freqWindow;
+    CaesarCipherWindow ccWindow;
+    JTextArea txtAreaCipher;
+    JTextArea txtAreaPlain;
+
+    JButton btnAnalysis;
+    JButton btnCipher;
 
     public static void main(String[] args){
         new Main();
@@ -27,12 +36,36 @@ public class Main extends JFrame{
     private void createChildWindows() {
         freqWindow = new FrequencyWindow(this);
         freqWindow.open();
+        ccWindow = new CaesarCipherWindow(this);
     }
 
     private void setUpGui(){
         JLabel lblHello = new JLabel("Hello World! Let's get cracking!");
         Container contentPane = this.getContentPane();
+        contentPane.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridheight = 2;
+
         contentPane.add(lblHello);
     }
 
+    private JPanel createToolButtonPanel(){
+        JPanel pnlToolButtons = new JPanel();
+        pnlToolButtons.setLayout(new GridLayout());
+        btnAnalysis = new JButton("Analysis Tools");
+        btnCipher = new JButton("Cipher solvers");
+
+        btnAnalysis.addActionListener(this);
+        btnCipher.addActionListener(this);
+
+        return pnlToolButtons;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
---
+++ src/visuals/CaesarCipherWindow.java
@@ -0,0 +1,18 @@
+package visuals;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CaesarCipherWindow extends JDialog  implements ActionListener {
+
+    public CaesarCipherWindow(Frame owner){
+        super(owner, "Caesar Cipher");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
