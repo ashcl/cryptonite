fa34586a435b82f6caad3428e146ff743001f686
ashcl <christopher.lee.ash@gmail.com>
2018-03-18 17:06:24 -0400

    Transposition Cipher implemented.

--- src/utilities/GeneralUtilities.java
+++ src/utilities/GeneralUtilities.java
@@ -1,6 +1,16 @@
 package utilities;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class GeneralUtilities {
+
+    public static void main(String[] args){
+        String key = "black";
+        int[] indexArray = obtainIndexKeyArray(key, true);
+        //Expected output: [1,4,0,2,3]
+        System.out.println(Arrays.toString(indexArray));
+    }
 
     /**
      * Pull out every (@param)length'th character starting at (@param)index and put it into a string
@@ -20,6 +30,35 @@ public class GeneralUtilities {
 
     public static String removeWhitespace(String s){
         return s.replaceAll("\\s+","");
+    }
+
+    public static int[] obtainIndexKeyArray(String key, boolean encoding){
+        char[] keyChars = key.toCharArray();
+        Arrays.sort(keyChars);
+        HashMap<Character, Integer> lastIndexMap = new HashMap<>();
+
+        int[] indexArray = new int[keyChars.length];
+        for (int i = 0; i < keyChars.length; i++) {
+            char keyChar = keyChars[i];
+            int index = findIndex(key, keyChar, lastIndexMap);
+
+            if (encoding){
+                indexArray[i] = index;
+            }else{
+                indexArray[index] = i;
+            }
+            lastIndexMap.put(keyChar, index);
+        }
+
+        return indexArray;
+    }
+
+    private static int findIndex(String key, char keyChar, HashMap<Character, Integer> lastIndexMap) {
+        if(lastIndexMap.containsKey(keyChar)) {
+            return key.indexOf(keyChar, lastIndexMap.get(keyChar));
+        }else{
+            return key.indexOf(keyChar);
+        }
     }
 
 }
--- src/utilities/TranspositionCipher.java
+++ src/utilities/TranspositionCipher.java
@@ -1,25 +1,63 @@
 package utilities;
 
+import java.util.ArrayList;
+
 public class TranspositionCipher {
 
     public static void main(String[] args){
-
+        String toEncode = "black";
+        String key = "black";
+        String encoded = encode(toEncode, key);
+        System.out.println(encoded);
+        System.out.println(decode(encoded, key));
     }
 
-    private static String transpose(String original, String key){
-        String stringInEdit = original.replace(" ", "");
-        stringInEdit = stringInEdit.toLowerCase();
-        char[] originalChars = stringInEdit.toCharArray();
-        char[] keyChars = key.toCharArray();
-
-
-
-        return null;
+    public static String encode(String s, String key){
+        int[] keyIndex = GeneralUtilities.obtainIndexKeyArray(key, true);
+        String plainText = GeneralUtilities.removeWhitespace(s);
+        //Split cipher text into blocks of key.length()
+        ArrayList<String> subStrings = new ArrayList<>();
+        for (int i = 0; i < plainText.length(); i= i+key.length()){
+            subStrings.add(extractStringBlock(plainText, key.length(), i));
+        }
+        StringBuilder builder = new StringBuilder();
+        for(String block: subStrings) {
+            //Transpose each block according to the keyIndex
+            //Add transformed block to the builder
+            builder.append(transpose(block, keyIndex));
+        }
+        return builder.toString();
     }
 
-    private static void swapChars(char[] array, int firstIndex, int secondIndex){
-        char temp = array[firstIndex];
-        array[firstIndex] = array[secondIndex];
-        array[secondIndex] = temp;
+    public static String decode(String s, String key){
+        int[] keyIndex = GeneralUtilities.obtainIndexKeyArray(key, false);
+        String cipherText = GeneralUtilities.removeWhitespace(s);
+        //Split cipher text into blocks of key.length()
+        ArrayList<String> subStrings = new ArrayList<>();
+        for (int i = 0; i < cipherText.length(); i= i+key.length()){
+            subStrings.add(extractStringBlock(cipherText, key.length(), i));
+        }
+        StringBuilder builder = new StringBuilder();
+        for(String block: subStrings) {
+            //Transpose each block according to the keyIndex
+            //Add transformed block to the builder
+            builder.append(transpose(block, keyIndex));
+        }
+        return builder.toString();
+    }
+
+    private static String extractStringBlock(String victim, int blockSize, int beginningIndex){
+        if((beginningIndex + blockSize) > victim.length()){
+            return victim.substring(beginningIndex);
+        }
+        return victim.substring(beginningIndex, beginningIndex + blockSize);
+    }
+
+    private static String transpose(String block, int[] keyIndex) {
+        StringBuilder transposeBuilder = new StringBuilder();
+        for (int i: keyIndex){
+            transposeBuilder.append(String.valueOf(block.charAt(i)));
+        }
+        return transposeBuilder.toString();
     }
 }
